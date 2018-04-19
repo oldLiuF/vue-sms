a64c3eea5382da19f7b39e8f8fa98e474324b0d9
oldLiuF <757542657@qq.com>
2018-04-19 23:11:57 +0800

    commit

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -9,13 +9,17 @@
           <el-menu  background-color="#409EFF" text-color="#fff" active-text-color="#ffd04b">
             <el-submenu index="1">
               <template slot="title">test1</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-1">
+                <router-link :to="list"></router-link>
+              </el-menu-item>
               <el-menu-item index="1-2">选项2</el-menu-item>
               <el-menu-item index="1-3">选项3</el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
-        <el-main></el-main>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
       </el-container>
     </el-container>
 </template>
@@ -46,4 +50,3 @@ export default {
     background-color: #409EFF
   }
 </style>
-
